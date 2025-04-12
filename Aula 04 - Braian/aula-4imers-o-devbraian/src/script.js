51327017ea396fd551bcac1f07776d79cae3a6cf
Braian-paC <braianbbg4@gmail.com>
2025-04-12 05:38:52 -0300

    Aula 04 corrigida

--- Aula 04 - Braian/aula-4imers-o-devbraian/src/script.js
+++ Aula 04 - Braian/aula-4imers-o-devbraian/src/script.js
@@ -1,36 +1,53 @@
 // Criar uma lista com 3 jogadores
 
-Time1 = ["", "", ""]
-Time2 = ["", "", ""]
+let Time1 = ["", "", ""]
+let Time2 = ["", "", ""]
 
 forcaTime1 = 0
 forcaTime2 = 0
 
 Time2_nomes = ["Magus", "Lavos", "Ozzie", "SCP-682 - The Reptyle", "Phoenix", "Kil'Jaeden", "Arthas", "Blademaster", "Slenderman", "Demon Hunter"]
 
-for (let i=0; i<3; i++){
-  escolhaPersonagem = prompt("Digite o nome do personagem "+(i+1)+": ")
-  Time1[i] = escolhaPersonagem
-  // Calcular a força de cada jogador e depois somar para saber a força do time.
+c = 1
+
+
+function Heroi_1() {
+  const heroInput = document.getElementById("HeroiInput")
+  const textoInput = heroInput.value
+  Time1[c-1] = textoInput
+  
   forcaTime1 += Math.floor(Math.random()*10)+1
   indiceAleatorio = Math.floor(Math.random()*10)
-  Time2[i] = Time2_nomes[indiceAleatorio]
+  Time2[c-1] = Time2_nomes[indiceAleatorio]
   forcaTime2 += Math.floor(Math.random()*10)+1
+  
+  c ++
+  document.getElementById("HeroiInput").value=""
+  
+  if (c > 3){
+    alert('Membros do seu Time: '+Time1[0]+", "+Time1[1]+" e "+Time1[2])
+    alert('Membros do Time do BOT: '+Time2[0]+", "+Time1[2]+" e "+Time2[2])
+    
+    if (forcaTime1 > forcaTime2) {
+      alert('Parabéns, você venceu!')
+      alert('Sua força: '+forcaTime1+"\nForça do BOT: "+forcaTime2)
+    }
+    else if (forcaTime2 > forcaTime1) {
+      alert('Perdeu pra um bot.')
+      alert('Sua força: '+forcaTime1+"\nForça do BOT: "+forcaTime2)
+    }
+    else{
+      alert('Por incrível que pareça deu empate! Ambos são igualmente fortes!')
+    }
+    
+    c = 1
+  }
 }
 
-alert("Agora o Time 1 contém os personagens: "+Time1)
+const inputElement = document.getElementById("HeroiInput")
 
-alert("Agora o Time 2 contém os personagens: "+Time2)
-// Comparar os dois times para saber qual deles venceu
-if (forcaTime1 > forcaTime2) {
-  alert("Parabéns, você venceu do time do computador!")
-  alert("Força do seu time: "+forcaTime1+"\nForça do time do computador: "+forcaTime2)
-}
-else if (forcaTime2 > forcaTime1) {
-  alert("Bom, infelizmente você perdeu para o computador.")
-  alert("Força do seu time: "+forcaTime1+"\nForça do time do computador: "+forcaTime2)
-}
-else {
-  alert("Por incrível que pareça, ambos os times são muito fortes!")
-  alert("Força de ambos os times: "+forcaTime1)
-}
+inputElement.addEventListener("keydown", function(event) {
+  if (event.key === "Enter") {
+    Heroi_1()
+  }
+})
